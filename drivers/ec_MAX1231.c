c9a2dd04919d1c012f930f6444cb98eb8fa370a3
raedovj <raedovj@hotmail.com>
2017-06-18 13:06:42 +0300

    finish MAX1231 configuration and add interrupt handler

--- drivers/ec_MAX1231.c
+++ drivers/ec_MAX1231.c
@@ -1,18 +1,75 @@
 #include "ec_MAX1231.h"
 #include <stdint.h>
 
+/*
+ * ADC is configured to read temp and AIN0...AIN14 = total of 16 channels.
+ * Reading initialized in ec_MAX1231_init() and re-inited in EOC interrupt
+ * Results are stored in cfg_table->data
+ */
+
+
+ec_MAX1231_cfg_t * cfg_t_ptr[3];
+
 void ec_MAX1231_init(ec_MAX1231_cfg_t *cfg_table)
 {
-    //Configure pins - hardcode
 
-    //configure pin interrupts - hardcode
+    if (cfg_table->index == ADC0)
+    {
+        //Configure nCNVST_pin
+        P8DIR |= BIT2; // output
+        P8OUT |= BIT2; // pull high
 
+        //configure nCS_pin
+        P2DIR |= BIT2; // output
+        P2OUT |= BIT2; // pull high
+
+        //Configure nEOC_pin with interrupt
+        P2DIR &= ~(BIT3); // input
+        P2IE |= BIT3; // interrupt enabled
+        P2IFG &= ~BIT3; // interrupt flag cleared
+
+        cfg_t_ptr[0] = cfg_table;
+    }
+    else if (cfg_table->index == ADC1)
+    {
+        //Configure nCNVST_pin
+        P8DIR |= BIT3; // output
+        P8OUT |= BIT3; // pull high
+
+         //configure nCS_pin
+        P8DIR |= BIT0; // output
+        P8OUT |= BIT0; // pull high
+
+        //Configure nEOC_pin with interrupt
+        P3DIR &= ~(BIT7); // input
+        P3IE |= BIT7; // interrupt enabled
+        P3IFG &= ~BIT7; // interrupt flag cleared
+
+        cfg_t_ptr[1] = cfg_table;
+    }
+    else if (cfg_table->index == ADC2)
+    {
+        //Configure nCNVST_pin
+        P8DIR |= BIT1; // output
+        P8OUT |= BIT1; // pull high
+
+        //configure nCS_pin
+        P3DIR |= BIT6; // output
+        P3OUT |= BIT6; // pull high
+
+        //Configure nEOC_pin with interrupt
+        P3DIR &= ~(BIT5); // input
+        P3IE |= BIT5; // interrupt enabled
+        P3IFG &= ~BIT5; // interrupt flag cleared
+
+        cfg_t_ptr[1] = cfg_table;
+    }
     //configure SPI
 
     //SPI writes to init MAX1231
     // Write to conversion register
-    uint8_t CHSEL_bits = 0000;  // 4 bits // AIN0
-    uint8_t SCAN_bits = 00;     // 2 bits // Scans channels 0 through N.
+    uint8_t CHSEL_bits = cfg_table->N_channels;  // 4 bits // Config how many channels are to be scanned
+    uint8_t SCAN_bits = 00;     // 2 bits // Scans channels 0 through N - selected by CHSEL3–CHSEL0.
     uint8_t TEMP_bits = 1;      // 1 bit  // reads temperature
     spi_write( (uint8_t) ( (1 << 7) | (CHSEL_bits << 3) | (SCAN_bits << 1) | (TEMP_bits) );
 
@@ -35,6 +92,8 @@ void ec_MAX1231_init(ec_MAX1231_cfg_t *cfg_table)
     uint8_t NSCAN_bits = 00;    // 2 bits // Single-channel scan count. (Scan mode 10 only.)
     spi_write( (uint8_t) ( (0b001 << 5) | (AVGON_bits << 4) | (NAVG_bits << 2) | (NSCAN_bits) );
     */
+
+
 }
 
 void ec_MAX1231_reset(ec_MAX1231_cfg_t *cfg_table)
@@ -45,6 +104,8 @@ void ec_MAX1231_reset(ec_MAX1231_cfg_t *cfg_table)
 
     // Write reset to ADC
     spi_write(0b00010000);
+    // Write reset FIFO to ADC
+    spi_write(0b00011000);
 
     //reset SPI
 
@@ -53,20 +114,68 @@ void ec_MAX1231_clear_FIFO(ec_MAX1231_cfg_t *cfg_table)
 {
     // Write reset FIFO to ADC
     spi_write(0b00011000);
-
 }
 
 
 void ec_MAX1231_read_results(ec_MAX1231_cfg_t *cfg_table, uint16_t *result)
 {
-    // maybe somehow add how many results should be read from FIFO... number ranges from 0...17
-    // ADC converison nr can be set with SCAN_bits
+    // atomic? or return pointer of cfg_table->data
+    for (uint8_t i = 0; i < cfg_table->N_channels; i++)
+    {
+        result[i] = cfg_table->data[i];
+    }
+}
 
-    // pull CNVST low for at least 40ns
+// Interrupt driven ADC reading (w DMA??)
+#pragma vector=PORT2_VECTOR //adc0 p2.3
+__interrupt void Port_2(void)
+{
+    if (P2IFG && BIT3)
+    {
+        P2OUT &= ~BIT2; // nCS
 
-    // wait for EOC to go low
+        spi_read(cfg_t_ptr[0]->data, nr_of_bytes);
 
-    // pull CS low and read data with spi
+        P2IFG &= ~BIT3; // interrupt flag cleared
+
+        P2OUT |= BIT2; //reset nCS
+
+        // toggle nCNSTV
+        P8OUT &= ~BIT2;
+        P8OUT |= BIT2;
+    }
 }
 
 // Interrupt driven ADC reading (w DMA)
+#pragma vector=PORT3_VECTOR //adc1 p3.7     adc2 p3.6
+__interrupt void Port_3(void)
+{
+    if (P3IFG && BIT7)
+    {
+        P8OUT &= 0BIT?; // nCS
+
+        spi_read(cfg_t_ptr[1]->data, nr_of_bytes);
+
+        P3IFG &= ~BIT7; // interrupt flag cleared
+
+        P8OUT |= BIT0; //reset nCS
+
+        // toggle nCNSTV
+        P8OUT &= ~BIT3;
+        P8OUT |= BIT3;
+    }
+    if (P3IFG && BIT6)
+    {
+        P3OUT &= ~BIT6; // nCS
+
+        spi_read(cfg_t_ptr[2]->data, nr_of_bytes);
+
+        P3IFG &= ~BIT6; // interrupt flag cleared
+
+        P3OUT |= BIT6; //reset nCS
+
+        // toggle nCNSTV
+        P8OUT &= ~BIT1;
+        P8OUT |= BIT1;
+    }
+}
--- drivers/ec_MAX1231.h
+++ drivers/ec_MAX1231.h
@@ -7,15 +7,23 @@
     CLK_MODE11
 } clock_mode;*/
 
+typedef enum {
+    ADC0,
+    ADC1,
+    ADC2
+} ADC_index;
+
 typedef struct {
     echal_spi_cfg_t spi_conf*; ///< SPI conf
 
     // Need GPIO.c first for proper pin configuration
-    IO_t nCNVST_pin; ///< data is requested if nCNVST is pulled low
+    /*IO_t nCNVST_pin; ///< data is requested if nCNVST is pulled low
     IO_t nEOC_pin; ///< data is valid if EOC pulls low. Triggers interrupt
-    IO_t nCS_pin; ///< Active-Low Chip Select Input. When CS is low, the serial interface is enabled.
+    IO_t nCS_pin; ///< Active-Low Chip Select Input. When CS is low, the serial interface is enabled.*/
 
-    uint16_t *data; ///< buffer where MAX1231 writes data. ## how big needs to be ? ##
+    uint16_t *data; ///< buffer where MAX1231 writes data. ## max size 16 ##
+    ADC_index index;
+    uint8_t N_channels; ///< number of channels to be read
 } ec_MAX1231_cfg_t;
 
 void ec_MAX1231_init(
@@ -31,6 +39,6 @@ void ec_MAX1231_clear_FIFO(
 );
 
 void ec_MAX1231_read_results(
-        ec_MAX1231_cfg_t *cfg_table, ///< [in] which ADC value to return
-        uint16_t *result ///< [out]
+        ec_MAX1231_cfg_t *cfg_table, ///< [in] to indicate which ADC to read
+        uint16_t *result ///< [out] ADC result(s)
 );
